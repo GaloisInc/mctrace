24606b9b2f4c3684323ae6217b65d4cea08294de
Hari Menon <hari@galois.com>
2023-01-25 03:41:44 -0500

    Additional test executable and probe.

---
+++ mctrace/tests/eval/fopen-calloc-fclose-probe.d
@@ -0,0 +1,15 @@
+long elapsed;
+long ts;
+long prog_cntr;
+
+::fopen:entry, ::fclose:entry, ::calloc:entry
+{
+    ts = timestamp;
+    prog_cntr = ucaller;
+}
+
+::fopen:return, ::fclose:return, ::calloc:return
+{
+    elapsed = timestamp - ts;
+    prog_cntr = ucaller;
+}
---
+++ mctrace/tests/full/alloc-dealloc-fread-fwrite.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE (4096 * 1024 * 100)
+
+char* target_file = "/tmp/dumpster.bin";
+
+int main() {
+  FILE* fi = fopen("/dev/random", "r");
+  FILE* fo = fopen(target_file, "w+");
+  fprintf(stderr, "%p, %p\n", fi, fo);
+  for(int i = 0; i < 4; i++) {
+    void* buf = calloc(BUF_SIZE, 1);
+    fread(buf, 1, BUF_SIZE, fi);
+    fwrite(buf, 1, BUF_SIZE, fo);
+    free(buf);
+  }
+  //fprintf(stderr, "%p, %p\n", fi, fo);
+  fclose(fi);
+  fclose(fo);
+  return 0;
+}
