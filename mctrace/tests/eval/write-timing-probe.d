1e7fe8574a17cb6c3c21c72ef52fd0aa6feafff0
Hari Menon <hari@galois.com>
2023-03-23 17:52:36 -0400

    Rename probe to be reflect what it actually does

--- mctrace/tests/eval/write-timing-probe.d
+++ mctrace/tests/eval/write-timing-probe.d
@@ -1,19 +1,15 @@
 int write_count;
-int write_return;
 long write_ts;
 long write_elapsed;
-long prog_cntr;
+
 
 ::write:entry {
   write_count = write_count + 1;
-  write_return = 0;
   write_ts = timestamp;
   write_elapsed = 0l;
-  prog_cntr = ucaller;
 }
 
 ::write:return {
   write_return = 1;
   write_elapsed = timestamp - write_ts;
-  prog_cntr = ucaller;
 }
