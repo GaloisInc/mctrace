035dc5a23afb36f0a0f75d55d8944d7afe568484
Jonathan Daugherty <jtd@galois.com>
2023-03-28 10:21:31 -0700

    build.sh: also build the PowerPC runtime

--- build.sh
+++ build.sh
@@ -4,8 +4,14 @@ set -e
 
 HERE=$(cd `dirname $0`; pwd)
 
+cd $HERE
+. env.sh
+
 cd $HERE/mctrace/tests/library/X86
 musl-gcc -o runtime.o -c runtime.c
+
+cd $HERE/mctrace/tests/library/PPC
+powerpc-linux-muslsf-gcc -o runtime.o -c runtime.c
 
 cd $HERE/mctrace/tests/full
 make
