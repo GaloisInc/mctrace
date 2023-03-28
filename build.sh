5901e4f5166e0c1ae135778938d04aa56993943c
Jonathan Daugherty <jtd@galois.com>
2023-03-28 10:43:48 -0700

    build.sh: comment steps

--- build.sh
+++ build.sh
@@ -7,15 +7,20 @@ HERE=$(cd `dirname $0`; pwd)
 cd $HERE
 . env.sh
 
+# Build the x86 runtime with the host compiler
 cd $HERE/mctrace/tests/library/X86
 musl-gcc -o runtime.o -c runtime.c
 
+# Build the powerpc runtime with the cross compiler built by
+# dev_setup.sh
 cd $HERE/mctrace/tests/library/PPC
 powerpc-linux-muslsf-gcc -o runtime.o -c runtime.c
 
+# Build stuff in the tests directory
 cd $HERE/mctrace/tests/full
 make
 
+# Build mctrace itself
 cd $HERE
 cabal configure pkg:mctrace
 cabal build :pkg:mctrace
