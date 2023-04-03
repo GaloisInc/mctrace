91123b60699b14ed63081771af712a8305d7488a
Jonathan Daugherty <jtd@galois.com>
2023-04-03 16:59:20 -0700

    build.sh: skip runtime library builds since the test makefile does that now

--- build.sh
+++ build.sh
@@ -7,15 +7,6 @@ HERE=$(cd `dirname $0`; pwd)
 cd $HERE
 . env.sh
 
-# Build the x86 runtime with the host compiler
-cd $HERE/mctrace/tests/library/X86
-musl-gcc -o runtime.o -c runtime.c
-
-# Build the powerpc runtime with the cross compiler built by
-# dev_setup.sh
-cd $HERE/mctrace/tests/library/PPC
-powerpc-linux-muslsf-gcc -o runtime.o -c runtime.c
-
 # Build mctrace itself
 cd $HERE
 cabal configure pkg:mctrace --enable-tests
