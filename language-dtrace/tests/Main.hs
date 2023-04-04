ae1bfd3af57c8b9caa667a6d0af4329b0e8a90be
Jonathan Daugherty <jtd@galois.com>
2023-04-04 15:01:05 -0700

    language-dtrace: add support for test cases for parsing

--- language-dtrace/tests/Main.hs
+++ language-dtrace/tests/Main.hs
@@ -7,6 +7,7 @@ import qualified Test.Tasty as TT
 
 import           Eval ( mkEvalTest )
 import           TypeCheck ( mkTypecheckTest )
+import           Parse ( mkParseTest )
 
 
 typecheckTests :: [FilePath] -> TT.TestTree
@@ -15,11 +16,16 @@ typecheckTests = TT.testGroup "TypeCheck" . map mkTypecheckTest
 evalTests :: [FilePath] -> TT.TestTree
 evalTests = TT.testGroup "Eval" . map mkEvalTest
 
+parseTests :: [FilePath] -> TT.TestTree
+parseTests = TT.testGroup "Parse" . map mkParseTest
+
 main :: IO ()
 main = do
+  parseTestExpected <- SFG.namesMatching "tests/parse/*.d.expected"
   tcTestExpected <- SFG.namesMatching "tests/tc/*.d.expected"
   evalTestExpected <- SFG.namesMatching "tests/eval/*.d.expected"
-  let tests = TT.testGroup "DTrace" [ typecheckTests tcTestExpected
+  let tests = TT.testGroup "DTrace" [ parseTests parseTestExpected
+                                    , typecheckTests tcTestExpected
                                     , evalTests evalTestExpected
                                     ]
   TT.defaultMain tests
