40da6ad9afe2edd792208940665c223b35aef709
Jonathan Daugherty <jtd@galois.com>
2023-04-14 11:39:10 -0700

    install-ppc-coreutils.sh: generalize to also build coreutils for ARM

--- scripts/install-ppc-coreutils.sh
+++ scripts/install-ppc-coreutils.sh
@@ -1,67 +1,111 @@
 #!/usr/bin/env bash
 
-# This script will download the sources for coreutils, patch it
-# appropriately and build a static version for PPC 32-bit
-# architecture and install the binaries in a specified location.
+# This script will download the source for coreutils, patch it
+# appropriately and build a statically-linked version for the each of
+# the architectures that we support and install the resulting binaries
+# in architecture-specific directories.
 
-# The script is expected to run after `dev_setup.sh` and expects
-# that the powerpc compiler is installed at the location specified
-# there. Currently the compiler should be at:
-# ../musl-gcc/output/bin/powerpc-linux-muslsf-gcc relative to this
-# script.
+# The script is expected to run after `dev_setup.sh` and expects that
+# the required cross compilers are installed at the locations specified
+# by the respective architecture-specific compiler variables at the
+# bottom of this script.
 
 set -e
 
 HERE=$(cd `dirname $0`; pwd)
-COMPILER=$(readlink -f "$HERE/../musl-gcc/output/bin/powerpc-linux-muslsf-gcc")
+
+LOG=$(readlink -m "$HERE/../install-coreutils.log")
+
+# The coreutils source that we are going to build
 COREUTILS_URL=https://github.com/coreutils/coreutils.git
 COREUTILS_REF=5891d28edebe229f1a6057275e281b10c1f2247b
 
-# Check for arguments
-if [[ -z "$1" ]]; then
-    echo "Usage $0 <installation path>"
-    echo
-    echo "The installation path should be the path to a (potentially"
-    echo "non-existent) directory that this script should create in"
-    echo "which the compiler will be installed."
-fi
-
-INSTALL_ROOT=$(readlink -f "$1")
-
-# Check for compiler
-if [[ ! -x "$COMPILER" ]]; then
-    echo "Error: unable to find powerpc compiler at $COMPILER"
-    echo "Did you run dev_setup.sh?"
-    exit 1
-fi
-
-# We will work in a temporary directory
-WORKING_DIR=$(mktemp -d)
-
-function remove_temp {
-    rm -rf $WORKING_DIR
+# Requires: LOG set to log file path.
+function logged {
+    if ! [ -z "$LOG" ]
+    then
+        mkdir -p `dirname $LOG`
+        echo $* >>$LOG
+        if ! $* >>$LOG 2>&1
+        then
+            echo
+            echo "An error occurred; please see $LOG"
+            echo "Here are the last 50 lines:"
+            tail -n 50 $LOG
+            exit 1
+        fi
+    else
+        $*
+    fi
 }
 
-trap remove_temp EXIT
+function notice {
+    echo "[NOTICE] $*"
+}
 
-cd $WORKING_DIR
-echo "Working in $WORKING_DIR"
+function install_coreutils {
+    local arch_name=$1
+    local compiler=$2
+    local install_path=$3
 
-# Clone the repo, check out a fixed repo
-git clone "$COREUTILS_URL"
+    # Check for compiler
+    if [[ ! -x "$compiler" ]]; then
+        echo "Error: unable to find compiler at $compiler"
+        echo "Did you run dev_setup.sh?"
+        exit 1
+    fi
 
-cd coreutils
-git checkout "$COREUTILS_REF"
+    notice "Found $arch_name compiler at $compiler"
 
-# NOTE: Our patch is really a single change. So we just use sed to do it.
-# If anything more complex is needed at some point, switch to a patch file
-sed -ri 's/(.*gl_WARN_ADD.*WERROR_CFLAGS.*)/\#\1/i' configure.ac
+    # We will work in a temporary directory
+    WORKING_DIR=$(mktemp -d)
 
-# Bootstrap to create a configure file
-./bootstrap
+    function remove_temp {
+        rm -rf $WORKING_DIR
+    }
 
-# Configure
-CC="$COMPILER" CFLAGS='-static' LDFLAGS='-static' ./configure --prefix="$INSTALL_ROOT"
+    trap remove_temp EXIT
 
-# Make and install
-make && make install
+    cd $WORKING_DIR
+
+    notice "Fetching coreutils source"
+
+    # Clone the repo, check out a fixed repo
+    logged git clone "$COREUTILS_URL"
+
+    cd coreutils
+    logged git checkout "$COREUTILS_REF"
+
+    # NOTE: Our patch is really a single change. So we just use sed to
+    # do it. If anything more complex is needed at some point, switch to
+    # a patch file
+    sed -ri 's/(.*gl_WARN_ADD.*WERROR_CFLAGS.*)/\#\1/i' configure.ac
+
+    notice "Building coreutils for $arch_name"
+
+    # Bootstrap to create a configure file
+    logged ./bootstrap
+
+    # Configure
+    CC="$compiler" CFLAGS='-static' LDFLAGS='-static' logged ./configure --prefix="$install_path"
+
+    # Create the installation directory
+    mkdir -p $install_path
+
+    # Make and install
+    logged make
+    logged make install
+
+    notice "coreutils for $arch_name installed at $install_path"
+}
+
+PPC_COMPILER=$(readlink -f "$HERE/../musl-gcc/output/bin/powerpc-linux-muslsf-gcc")
+PPC_PATH=$(readlink -m "$HERE/../coreutils/ppc")
+
+ARM_COMPILER=$(readlink -f "$HERE/../musl-gcc-arm/output/bin/arm-linux-musleabi-gcc")
+ARM_PATH=$(readlink -m "$HERE/../coreutils/arm32")
+
+notice "Logging build output to $LOG"
+
+install_coreutils "PowerPC" $PPC_COMPILER $PPC_PATH
+install_coreutils "arm32" $ARM_COMPILER $ARM_PATH
