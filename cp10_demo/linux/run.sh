b25631fed8f0cfd61f0e699eb4d8299182daf32e
Daniel Matichuk <dmatichuk@galois.com>
2023-07-07 13:25:15 -0700

    move cp10_demo into linux subdirectory
    This is to disambiguate between the user-space and bare-metal binaries

--- cp10_demo/linux/run.sh
+++ cp10_demo/linux/run.sh
@@ -7,4 +7,4 @@ HERE=$(cd `dirname $0`; pwd)
 cd $HERE
 rm -f program_c_write.bin
 ./cp10_program_c_ppc32.mctrace 2>telemetry.bin
-../scripts/extractor.py program_c_mapping.json --big-endian --columns < telemetry.bin
+../../scripts/extractor.py program_c_mapping.json --big-endian --columns < telemetry.bin
