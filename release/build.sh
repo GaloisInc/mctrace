269a67b1292079961a07e9691998375560c229f1
Jonathan Daugherty <jtd@galois.com>
2023-07-25 11:23:17 -0700

    release/build.sh: also build the tool-only image

--- release/build.sh
+++ release/build.sh
@@ -6,5 +6,10 @@ HERE=$(cd `dirname $0`; pwd)
 cd $HERE
 
 cd $HERE/release
+
+# Build the main release image
 sudo docker build --no-cache -t mctrace .
 sudo docker save mctrace:latest | gzip > mctrace.tar.gz
+
+# Build the tool-only image
+sudo docker build - -f Dockerfile-for-Tool -t mctrace-tool
